676b184c13498f446115a8e3227e223fbf28b834
BraCatal <brenocat@tutamail.com>
2025-06-12 10:19:09 -0300

    Update AquarioDTO.java

--- src/main/java/com/example/Aquarium/dto/AquarioDTO.java
+++ src/main/java/com/example/Aquarium/dto/AquarioDTO.java
@@ -1,16 +1,53 @@
 package com.example.Aquarium.dto;
 
-import lombok.Data;
-
 import java.util.List;
 
-@Data
 public class AquarioDTO {
-
     private Long id;
     private String nome;
     private String localizacao;
     private Integer capacidade;
-    private List<AnimalDTO> animais; // Lista de animais associada ao aquário
+    private List<AnimalDTO> animais; // Lista de animais que pertencem a esse aquário
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    public Integer getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Integer capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public List<AnimalDTO> getAnimais() {
+        return animais;
+    }
+
+    public void setAnimais(List<AnimalDTO> animais) {
+        this.animais = animais;
+    }
 }
+
 
