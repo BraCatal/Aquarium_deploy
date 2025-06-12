cea607cbd1d2b13c92522f414492adfef24ba9ca
BraCatal <brenocat@tutamail.com>
2025-06-12 10:17:34 -0300

    Update AnimalDTO.java

--- src/main/java/com/example/Aquarium/dto/AnimalDTO.java
+++ src/main/java/com/example/Aquarium/dto/AnimalDTO.java
@@ -1,13 +1,50 @@
 package com.example.Aquarium.dto;
 
-import lombok.Data;
-
-@Data
 public class AnimalDTO {
-
     private Long id;
     private String nome;
     private String especie;
     private Integer idade;
+    private Long aquarioId; // Novo campo para o relacionamento
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Long getAquarioId() {
+        return aquarioId;
+    }
+
+    public void setAquarioId(Long aquarioId) {
+        this.aquarioId = aquarioId;
+    }
 }
 
