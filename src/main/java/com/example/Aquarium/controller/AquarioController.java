00e4862aa0284eac35a9be9964b3e7c4a459e8c7
BraCatal <brenocat@tutamail.com>
2025-06-12 08:53:20 -0300

    Update AquarioController.java

--- src/main/java/com/example/Aquarium/controller/AquarioController.java
+++ src/main/java/com/example/Aquarium/controller/AquarioController.java
@@ -1,6 +1,8 @@
 package com.example.Aquarium.controller;
 
 import com.example.Aquarium.dto.AquarioDTO;
+import com.example.Aquarium.service.AquarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,25 +11,30 @@ import java.util.List;
 @RequestMapping("/aquario")
 public class AquarioController {
 
+    @Autowired
+    private AquarioService aquarioService;
+
     @GetMapping
     public List<AquarioDTO> listar() {
-        return null;
+        return aquarioService.listar();
     }
 
     @PostMapping
     public AquarioDTO criar(@RequestBody AquarioDTO aquarioDTO) {
-        return null;
+        return aquarioService.criar(aquarioDTO);
     }
 
     @PutMapping("/{id}")
     public AquarioDTO atualizar(@PathVariable Long id, @RequestBody AquarioDTO aquarioDTO) {
-        return null;
+        return aquarioService.atualizar(id, aquarioDTO);
     }
 
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable Long id) {
+        aquarioService.deletar(id);
     }
 }
+
 
 
 
