a1ff982592fee253a70756cfdeee5a89bfb6ac58
BraCatal <brenocat@tutamail.com>
2025-06-12 10:13:05 -0300

    Update AnimalMapper.java

--- src/main/java/com/example/Aquarium/mapper/AnimalMapper.java
+++ src/main/java/com/example/Aquarium/mapper/AnimalMapper.java
@@ -2,6 +2,7 @@ package com.example.Aquarium.mapper;
 
 import com.example.Aquarium.dto.AnimalDTO;
 import com.example.Aquarium.model.Animal;
+import com.example.Aquarium.model.Aquario;
 
 public class AnimalMapper {
 
@@ -13,5 +14,17 @@ public class AnimalMapper {
         dto.setIdade(animal.getIdade());
         return dto;
     }
+
+    public static Animal toEntity(AnimalDTO dto) {
+        Animal animal = new Animal();
+        animal.setId(dto.getId());
+        animal.setNome(dto.getNome());
+        animal.setEspecie(dto.getEspecie());
+        animal.setIdade(dto.getIdade());
+
+        // Obs: aqui não associamos aquário diretamente, isso pode ser feito no Service
+        return animal;
+    }
 }
+
 
