4c60c8ceb36f4b689965fa6b8e82a9c5c5b223e2
BraCatal <brenocat@tutamail.com>
2025-06-13 19:55:20 -0300

    Update AnimalRepository.java

--- src/main/java/com/example/Aquarium/repository/AnimalRepository.java
+++ src/main/java/com/example/Aquarium/repository/AnimalRepository.java
@@ -1,7 +1,8 @@
 package com.example.Aquarium.repository;
 
-import com.example.Aquarium.model.Animal;
+import com.example.Aquarium.model.Aquario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
+public interface AquarioRepository extends JpaRepository<Aquario, Long> {
 }
+
